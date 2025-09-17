9f2e10e3123f114f9895eeb12298b50b5e05ab9f
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2025-09-17 11:20:44 -0400

    added register and clear functionality

--- src/lab_03/Lab_03.java
+++ src/lab_03/Lab_03.java
@@ -5,14 +5,18 @@
 package lab_03;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -56,6 +60,37 @@ public class Lab_03 extends Application {
         grid.add(password, 0, 3);
         PasswordField passwordField = new PasswordField();
         grid.add(passwordField, 1, 3);
+        
+        Button register = new Button("Register");
+        register.setDisable(false);
+        grid.add(register, 0, 4);
+        
+        Text loginStatus = new Text();
+        grid.add(loginStatus, 0, 5);
+        
+        register.setOnAction((ActionEvent t) -> {
+            boolean validEmail = emailField.getText().contains("@");
+            
+            boolean passwordContainsLetter = passwordField.getText().matches(".*[a-zA-Z].*");
+            boolean passwordContainsNumber = passwordField.getText().matches(".*[0-9].*");
+            boolean validPassword = passwordContainsLetter && passwordContainsNumber;
+            
+            if (validEmail && validPassword) {
+                loginStatus.setText("Success!");
+            } else {
+                loginStatus.setText("Invalid.");
+            }
+        });
+        
+        Button clear = new Button("Clear");
+        grid.add(clear, 1, 4);
+        
+        clear.setOnAction((ActionEvent t) -> {
+            firstNameField.setText("");
+            lastNameField.setText("");
+            emailField.setText("");
+            passwordField.setText("");
+        });
         
         Scene scene = new Scene(root, 300, 275);
         stage.setScene(scene);
